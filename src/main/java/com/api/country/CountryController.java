c4eef096eafd43e1b9ab27865eb038c86554b02f
Andre Yang <doninyangm@gmail.com>
2020-07-14 11:53:37 +0100

    country index endpoint

--- src/main/java/com/api/country/CountryController.java
+++ src/main/java/com/api/country/CountryController.java
@@ -1,5 +1,22 @@
 package com.api.country;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CountryController {
+	
+	@Autowired
+	private CountryDAO service;
+	
+	@GetMapping("/")
+	public List<Country> index() {
+		return service.findAll();
+	}
 
 }
--- src/main/java/com/api/country/CountryDAO.java
+++ src/main/java/com/api/country/CountryDAO.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class CountryDAO {
 	
 	private static List<Country> countries = new ArrayList<>();
